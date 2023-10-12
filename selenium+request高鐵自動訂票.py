3fea4d29376bcbffa81ea4a125ac471ff72d1025
Alex Chen <s55667788991010@gmail.com>
2023-10-12 15:55:12 +0800

    改寫為物件導向方式

--- selenium+request高鐵自動訂票.py
+++ selenium+request高鐵自動訂票.py
@@ -8,117 +8,108 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 
 
-# 查詢高鐵票
+# 設定目前時間
+now_time = time.time()
+struct_time = time.localtime(now_time) # 轉成時間元組
+timeString = time.strftime("%Y-%m-%d %H:%M:%S", struct_time) # 轉成字串
 
-# start_station = input("請輸入上車站:")
-# end_station = input("請輸入下車站:")
-# date = input("請輸入乘車日期:")
-# time = input("請輸入乘車時間")
+now_time_hour = timeString.split()[1].split(":")[0]
+today = timeString.split()[0].replace("-","/")
 
-station_dict = {
-    "南港":"NanGang",
-    "台北":"TaiPei",
-    "板橋":"BanQiao",
-    "桃園":"TaoYuan",
-    "新竹":"HsinChu",
-    "苗栗":"MiaoLi",
-    "台中":"TaiChung",
-    "彰化":"ChangHua",
-    "雲林":"YunLin",
-    "嘉義":"ChiaYi",
-    "台南":"TaiNan",
-    "左營":"ZuoYing",
-}
-
-
-# def return_time(time):
-#     if int(time.split(":")[1]) <30:
-#         return str(int(time.split(":")[0])-1)+":30"
-#     else:
-#         return time.split(":")[0]+":00"
- 
-url = 'https://www.thsrc.com.tw/TimeTable/Search'
-
-# data = {
-#         "SearchType": "S",
-#         "Lang": "TW",
-#         "StartStation": station_dict[start_station],
-#         "EndStation": station_dict[end_station],
-#         "OutWardSearchDate": date,
-#         "OutWardSearchTime": time,
-#         "ReturnSearchDate": date,
-#         "ReturnSearchTime": time,
-#         "DiscountType":"" 
-# }
+# post請求資料
 data = {
-        "SearchType": "S",
-        "Lang": "TW",
-        "StartStation": "NanGang",
-        "EndStation": "TaiNan",
-        "OutWardSearchDate": "2023/09/29",
-        "OutWardSearchTime": "8:00",
-        "ReturnSearchDate": "2023/09/29",
-        "ReturnSearchTime": "8:00",
-        "DiscountType":"" 
+        'SearchType': 'S', 
+        'Lang': 'TW', 
+        'StartStation': 'NanGang', 
+        'EndStation': 'TaiNan', 
+        'OutWardSearchDate': '2023/10/15', 
+        'OutWardSearchTime': '8:00', 
+        'ReturnSearchDate': '2023/10/15', 
+        'ReturnSearchTime': '8:00', 
+        'DiscountType': ''
 }
-timee = "8:00"
-headers = {":authority":"www.thsrc.com.tw",
-           ":method":"POST",
-           ":path":"/TimeTable/Search",
-           ":scheme":"https",
-           "Accept":"*/*",
-           "Accept-Encoding":"gzip, deflate, br",
-           "Accept-Language":"en-US,en;q=0.9",
-           "Content-Length":"190",
-           "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
-           "Cookie":"_gcl_au=1.1.692045517.1695777542; _td_cid=1741007328.1695777542; _fbp=fb.2.1695777542642.547995837; _gid=GA1.3.119633726.1695777543; _gat_UA-9967381-26=1; AcceptThsrcCookiePolicyTime=Wed%20Sep%2027%202023%2009:19:04%20GMT+0800%20(%E5%8F%B0%E5%8C%97%E6%A8%99%E6%BA%96%E6%99%82%E9%96%93); _ga_1FDVRGS3MR=GS1.1.1695777542.1.1.1695777558.44.0.0; _ga_6M07CCJT7N=GS1.1.1695777542.1.1.1695777559.43.0.0; _ga=GA1.3.178527844.1695777542",
-           "Origin":"https://www.thsrc.com.tw",
-           "Referer":"https://www.thsrc.com.tw/ArticleContent/a3b630bb-1066-4352-a1ef-58c7b4e8ef7c",
-           "Sec-Ch-Ua":'"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-           "Sec-Ch-Ua-Mobile":"?0",
-           "Sec-Ch-Ua-Platform":"Windows",
-           "Sec-Fetch-Dest":"empty",
-           "Sec-Fetch-Mode":"cors",
-          " Sec-Fetch-Site":"same-origin",
-           "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-           "X-Requested-With":"XMLHttpRequest"
-           }
 
-respones = requests.post(url, data)
-print(respones.status_code)
-data = json.loads(respones.text)
+# 站名轉換
+station_dict = {
+        "南港":"NanGang",
+        "台北":"TaiPei",
+        "板橋":"BanQiao",
+        "桃園":"TaoYuan",
+        "新竹":"HsinChu",
+        "苗栗":"MiaoLi",
+        "台中":"TaiChung",
+        "彰化":"ChangHua",
+        "雲林":"YunLin",
+        "嘉義":"ChiaYi",
+        "台南":"TaiNan",
+        "左營":"ZuoYing",
+}
 
-idx = 0
-tb = pt.PrettyTable(["序列","車次","出發時間","抵達時間","乘車時間","自由座車廂","優惠類型"], encoding="utf-8")
+# 建立爬蟲類別
+class Taiwan_highway():
 
-endStationName = data["data"]["DepartureTable"]["Title"]["EndStationName"]
-startStationName = data["data"]["DepartureTable"]["Title"]["StartStationName"]
-titleSplit2 = data["data"]["DepartureTable"]["Title"]["TitleSplit2"]
+    def __init__(self, departure, destination, date=today):
+        self.departure = departure
+        self.destination = destination
+        self.date = date
+        self.url = 'https://www.thsrc.com.tw/TimeTable/Search'
+        self.dict = station_dict 
 
-data_index = {}
-for i in range(61):     
-        departureTime = data["data"]["DepartureTable"]["TrainItem"][i]['DepartureTime']
-        destinationTime = data["data"]["DepartureTable"]["TrainItem"][i]['DestinationTime']
-        duration = data["data"]["DepartureTable"]["TrainItem"][i]['Duration']
-        runDate = data["data"]["DepartureTable"]["TrainItem"][i]['RunDate']
-        trainNumber = data["data"]["DepartureTable"]["TrainItem"][i]["TrainNumber"]
-        no_reserved = data["data"]["DepartureTable"]["TrainItem"][i]["NonReservedCar"]  
+    def get_timetable(self):
         
-        # print(int(departureTime.split(':')[0]))
-        if int(departureTime.split(':')[0]) > int(timee.split(":")[0]):
+        data["StartStation"] = self.dict[self.departure]
+        data["EndStation"] = self.dict[self.destination]
+        data["OutWardSearchDate"] = self.date
+        data["ReturnSearchDate"] = self.date
+
+        respones = requests.post(self.url, data)
+        if respones.status_code == 200:
+            print("查詢成功")
+        date_info = json.loads(respones.text)
+        idx = 0
+        tb = pt.PrettyTable(["序列","車次","出發時間","抵達時間","乘車時間","自由座車廂","優惠類型"], 
+                            encoding="utf-8"
+                            )
+
+        
+        data_index = {}
+        for i in range(61):     
+            departureTime = date_info["data"]["DepartureTable"]["TrainItem"][i]['DepartureTime']
+            destinationTime = date_info["data"]["DepartureTable"]["TrainItem"][i]['DestinationTime']
+            duration = date_info["data"]["DepartureTable"]["TrainItem"][i]['Duration']
+            runDate = date_info["data"]["DepartureTable"]["TrainItem"][i]['RunDate']
+            trainNumber = date_info["data"]["DepartureTable"]["TrainItem"][i]["TrainNumber"]
+            no_reserved = date_info["data"]["DepartureTable"]["TrainItem"][i]["NonReservedCar"]
+            
+            if int(departureTime.split(':')[0]) > int(now_time_hour):
                 data_index[idx] = [departureTime,destinationTime,duration,runDate,trainNumber]
                 tb.add_row([idx, trainNumber, departureTime, destinationTime, duration, no_reserved, ""])
                 idx += 1
-        if idx == 10:
-            break
-print(tb.get_string(title=f"{startStationName}-{endStationName}  乘車日期:{titleSplit2}"))    
+            if idx == 10:
+                break
+        
+        return tb.get_string(title=f"{self.departure}-{self.destination}  乘車日期:{self.date}")
 
+
+
+
+# 查詢高鐵票
+startstation = input("請輸入上車站:")
+endstation = input("請輸入下車站:")
+
+# 可以自行填入要的日期
+titme_table = Taiwan_highway(startstation, endstation)
+
+tb = titme_table.get_timetable()
+print(tb)
+
+# 訂票 取票人資訊 身分證字號 連絡電話 電子郵件
 your_order = input("請輸入你要的班次(序列號):")
 pessenger_id = input("請輸入你的身分證字號:")
 phone_number = input("請輸入你的電話號碼:")
 your_email = input("請輸入你的電子郵件:")
 
-# 訂票 取票人資訊 身分證字號 連絡電話 電子郵件
+
 
 option = webdriver.FirefoxOptions()
 # option.add_experimental_option("excludeSwiches", ["enable-automation"])
